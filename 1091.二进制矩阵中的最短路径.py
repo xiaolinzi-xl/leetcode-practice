8713fbbacc8055182de7a94bef37eca4a0519944
xiaolinzi-xl <daoshangxl@163.com>
2020-07-08 21:48:54 +0800

    简化代码

--- 1091.二进制矩阵中的最短路径.py
+++ 1091.二进制矩阵中的最短路径.py
@@ -12,23 +12,24 @@ class Solution:
         n = len(grid)
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
+        if n == 1:
+            return 1
         visit_queue = queue.Queue()
         ans = -1
         visit_queue.put(((0, 0), 1))
         directions = ((-1, 0), (-1, 1), (0, 1), (1, 1),
                       (1, 0), (1, -1), (0, -1), (-1, -1))
-        visit_set = set()
+        visit_array = [[0] * n for _ in range(n)]
+        visit_array[0][0] = 1
         while not visit_queue.empty():
             idx, step = visit_queue.get()
-            if idx[0] == n-1 and idx[1] == n-1:
-                if grid[idx[0]][idx[1]] == 0:
-                    ans = step
-                break
             for direction in directions:
                 x, y = idx[0] + direction[0], idx[1] + direction[1]
-                if n > x >= 0 and n > y >= 0 and grid[x][y] == 0 and (x, y) not in visit_set:
+                if n > x >= 0 and n > y >= 0 and grid[x][y] == 0 and visit_array[x][y] == 0:
                     visit_queue.put(((x, y), step+1))
-                    visit_set.add((x, y))
+                    visit_array[x][y] = 1
+                    if x == n-1 and y == n-1:
+                        return step + 1
         return ans
 
 # @lc code=end
